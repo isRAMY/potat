1e0d3e985f3993d8497541a5c9413646cdf974b9
isRAMY <134105669+isRAMY@users.noreply.github.com>
2023-12-14 12:06:39 +0300

    Done By Ramy

---
+++ README.md
@@ -0,0 +1,1 @@
+[POTATOES](tg://openmessage?user_id=5844358773)
---
+++ StringGenBot/start.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+from config import OWNER_ID
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""Hᴇʏ {msg.from_user.mention},
+
+Tʜɪs ɪs {me2},
+Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+
+Mᴀᴅᴇ ʙʏ : [POTATOES](tg://user?id={OWNER_ID}) !""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/lucithon"),
+                    InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴩᴇʀ", user_id=OWNER_ID)
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
